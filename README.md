00cc037747ad22837629cde4db9b33d8fcfff5e0
Callie Stump <callie@stu.mp>
2020-11-06 16:53:14 -0500

    adds elements to readme

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# Title
+# Pizza Parlor
 
-#### **Date**
+#### **11.06.20**
 
 ## By _Callie Stump_
 ---
 ## **Description**
-
+This website takes a user to a Pizza Parlor webpage. The user gets asked for pizza size, topping, and contact information. Once order is submitted, a reciept is displayed to the screen.
 
 ## **Setup**
-1. Copy this download link: 
+1. Copy this download link: https://github.com/calliestump/Friday4PizzaParlor.git
 2. Open bash and go to the directory where you would like to store your cloned project.
 3. Clone the repo.
 ```
-git clone ""
+git clone "https://github.com/calliestump/Friday4PizzaParlor.git"
 ```
 4. Navigate to the cloned project folder and open VS code.
 ```
@@ -25,11 +25,18 @@ $ code .
 git remote add origin [repo. link]
 ```
 ## **Technologies Used**
+* HTML
+* CSS
+* Bootstrap
+* javaScript
+* jQuery
+* GIT
+* GitHub
 
 ## **Specs**
 
 ```
-Describe:
+Describe: 
 Test: 
 Expect 
 ```
--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,19 @@ function Pizza(size, toppings) {
   this.toppings = toppings;
   this.size = size;
 }
-
+/*Pizza.prototype.cost = function (){
+  var cost = {
+    cheese: 1.00,
+    bacon: 1.00,
+    sausage: 1.00
+  }
+}*/
+Pizza.prototype.cost = function (){
+  var cost = 0;
+  if (this.size === "medium") {
+    cost + 9;
+  }
+}
 //User
 //cost calc.
 var makePizza = function() {
@@ -16,13 +28,15 @@ var makePizza = function() {
       }
     }
     //puts sizes in array
-    var userSselect = document.getElementsByName("pizzaSize");
+    var userSselect = (document.getElementsByName("pizzaSize"));
+    //alert(this.size);
     var checkedBoxes2 = [];
     for (var i=0; i<userSselect.length; i++) {
       if (userSselect[i].checked) {
         checkedBoxes2.push(userSselect[i].value);
       }
     }  
+    JSON.stringify(checkedBoxes2);
   var size = $("input[name='pizzaSize']:checked").val();
   var toppings = $("input[name='toppingOption']:checked").map(function() {
     return $(this).val();
@@ -40,7 +54,9 @@ $(document).ready(function() {
   $("form#parlor").submit(function(event) {
     event.preventDefault();
     makePizza();
+    cost(size);
     //Reciept();
+    console.log(cost);
     $(".jumbotron-reciept").show();
     //const name = $("input#name").val();
     //const size = $("input[name='pizzaSize']:checked").val();
