0491507d7611b771c324b22363f24498f6b6dbe0
Callie Stump <callie@stu.mp>
2020-11-06 13:12:58 -0500

    adds and tests make pizza method

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
+function Pizza(size, toppings) {
+  this.toppings = toppings;
+  this.size = size;
+}
+
+//User
+//cost calc.
+var makePizza = function() {
+  var size = $("input[name='pizzaSize']:checked").val();
+  var toppings = $("input[name='toppingOption']:checked").map(function() {
+    return $(this).val();
+  });
+  var madePizza = new Pizza(size, toppings);
+  console.log(toppings);
+  alert(size);
+  console.log(madePizza);
+  return madePizza;
+};
+
+
 $(document).ready(function() {
-  $("form#").submit(function(event) {
+  $("form#parlor").submit(function(event) {
     event.preventDefault();
+    makePizza();
+    $(".reciept").show();
+    const name = $("input#name").val();
+
+    //const size = $("input[name='pizzaSize']:checked").val();
+    //const selectedToppings = $("input[name='pizzaSize']:checked").val();
   });
 });
