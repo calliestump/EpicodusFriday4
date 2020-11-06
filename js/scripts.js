49625c5d8349e992954c875b43c8d12642b6fc77
Callie Stump <callie@stu.mp>
2020-11-06 18:07:28 -0500

    orangizes variables to local with use of this

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 function Pizza(size, toppings) {
   this.toppings = toppings;
   this.size = size;
@@ -9,57 +8,57 @@ function Pizza(size, toppings) {
     bacon: 1.00,
     sausage: 1.00
   }
+
 }*/
-Pizza.prototype.cost = function (){
+/*Pizza.prototype.cost = function (){
   var cost = 0;
   if (this.size === "medium") {
     cost + 9;
   }
-}
+  this.cost = cost;
+}*/
 //User
 //cost calc.
-var makePizza = function() {
-  //puts toppings in array
-  var userTselect = document.getElementsByName("toppingOption");
-    var checkedBoxes = [];
-    for (var i=0; i<userTselect.length; i++) {
-      if (userTselect[i].checked) {
-        checkedBoxes.push(userTselect[i].value);
-      }
+
+Pizza.prototype.makePizza = function() {
+  this.toppings = document.getElementsByName("toppingOption");
+  //this.toppings = userToppings;
+  var checkedBoxes1 = [];
+  for (var i=0; i<this.toppings.length; i++) {
+    if (this.toppings[i].checked) {
+      checkedBoxes1.push(this.toppings[i].value);
     }
-    //puts sizes in array
-    var userSselect = (document.getElementsByName("pizzaSize"));
-    //alert(this.size);
-    var checkedBoxes2 = [];
-    for (var i=0; i<userSselect.length; i++) {
-      if (userSselect[i].checked) {
-        checkedBoxes2.push(userSselect[i].value);
-      }
-    }  
-    JSON.stringify(checkedBoxes2);
-  var size = $("input[name='pizzaSize']:checked").val();
-  var toppings = $("input[name='toppingOption']:checked").map(function() {
-    return $(this).val();
-  });
-  var madePizza = new Pizza(size, toppings);
-  $(".finalToppings").last().append(checkedBoxes + "");
-  $(".finalSize").append(checkedBoxes2);
+  }
+  //var userSize = document.getElementsByName("pizzaSize");
+  this.size = $("input[name='pizzaSize']:checked").val(); 
+  var checkedBoxes2 = [];
+  this.cost = 0;
+  for (var i=0; i<this.size.length; i++) {
+    if (this.size[i].checked) {
+      checkedBoxes2.push(this.size[i].value);
+    }
+  }  
+  if (this.size == "medium") {
+    this.cost +1;
+  }
+ reciept
+  $(".finalToppings").last().append(checkedBoxes1 + "");
+  $(".finalSize").append(this.size);
   $(".name").append($(".userName").val());
   $(".number").append($(".userNumber").val());
-  console.log(madePizza);
-  return madePizza;
-};
+  //console.log(madePizza);
+  //return madePizza;
+  if (userSize == "medium") {
+    this.cost +1;
+  }
+}
 
 $(document).ready(function() {
   $("form#parlor").submit(function(event) {
-    event.preventDefault();
-    makePizza();
-    cost(size);
-    //Reciept();
-    console.log(cost);
     $(".jumbotron-reciept").show();
-    //const name = $("input#name").val();
-    //const size = $("input[name='pizzaSize']:checked").val();
-    //const selectedToppings = $("input[name='pizzaSize']:checked").val();
+    event.preventDefault();
+    Pizza.prototype.makePizza();
+    alert(this.cost);
+    console.log(cost);
   });
 });
